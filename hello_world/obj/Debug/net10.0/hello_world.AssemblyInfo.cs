a569a4441a5eaaae64e91a3cfb2905842bf45cd3
kyungmin.kim <kyungmin.kim@techwing.co.kr>
2026-04-18 20:52:36 +0900

    obj update?

--- hello_world/obj/Debug/net10.0/hello_world.AssemblyInfo.cs
+++ hello_world/obj/Debug/net10.0/hello_world.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2634d5d8803ebaf77ed5854215833129ebb775a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb2ad6fc5726ec2e909a1e900191f5941d6583a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
